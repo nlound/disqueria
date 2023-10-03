4e87995c0701c7d0aca252ef964ba4b19ac8e4b6
Nicolas Lound <nicolas.lound@mercadolibre.com>
2023-10-03 20:13:51 -0300

    resolved conflics

--- index.php
+++ index.php
@@ -8,38 +8,81 @@
 
 <body>
   <?php include '_inc/datos.php'; ?>
+  <div class="promo-block">
+    <img src="images/icono-delivery.png" alt="Camión de delivery" class="me-3">
+    <p class="m-0 p-0">Envio gratis con tu comprar superior a $25.000</p>
+  </div>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <img src="img/logo.png" alt="Logo" class="img-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" disabled href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#seccion-1">Seccion 1</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#seccion-2">Seccion 2</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#seccion-3">Seccion 3</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#seccion-4">El local</a>
-          </li>
-        </ul>
-      </div>
+  <!--Header Desktop -->
+  <header class="py-3 only-desktop">
+    <div class="container d-flex flex-wrap justify-content-center align-items-center">
+      <a href="index.html" class="d-flex align-items-center mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+        <h1>
+          <span class="hidden--visually">Rkive Records</span>
+          <img class="rr-logo" src="images/rr-logo.png" alt="Logo de Rkive Records">
+        </h1>
+      </a>
+      <nav class="">
+        <div class="container d-flex flex-wrap">
+          <ul class="nav me-auto">
+            <li class="nav-item"><a href="#discoMes" class="nav-link link-secondary px-2 active"
+                aria-current="page">Disco del Mes</a></li>
+            <li class="nav-item nav-bar-links"><a href="#store" class="nav-link link-body-emphasis px-2"><img
+                  class="icono-disco me-2" src="images/icono-disco.png" alt="Icono de disco"><strong>Comprar
+                  mi
+                  disco</strong><img class="icono-disco id-inverted ms-2" src="images/icono-disco.png"
+                  alt="Icono de disco"></a></li>
+            <li class="nav-item nav-bar-links"><a href="#about" class="nav-link link-secondary px-2">Rkive Records</a>
+            </li>
+            <li class="nav-item nav-bar-links"><a href="#newsletter" class="nav-link link-secondary px-2">Newsletter</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  </nav>
+  </header>
+  <!--Header Desktop-->
+
+  <!--Header Mobile-->
+  <header class="only-mobile">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand no-pm" href="index.html">
+          <h1>
+            <span class="hidden--visually">Rkive Records</span>
+            <img class="rr-logo" src="images/bc-logo.png" alt="Logo de Rkive Records">
+          </h1>
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+          aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#discoMes">Disco del Mes</a>
+            </li>
+            <li class="nav-item nav-bar-links"><a href="#store" class="nav-link link-body-emphasis px-2"><img
+                  class="icono-disco me-2" src="images/icono-disco.png" alt="Icono de disco"><strong>Comprar
+                  mi
+                  disco</strong><img class="icono-disco ms-2" src="images/icono-disco.png" alt="Icono de disco"></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">Rkive Records</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#newsletter">Newsletter</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!--Header Mobile-->
+
 
   <div class="container" id="seccion-1">
-
     <div class="row">
       <div class="col-12 col-md-6 col-lg-4">COLUMNA</div>
       <div class="col-12 col-md-6 col-lg-4">COLUMNA</div>
@@ -65,22 +108,28 @@
   </div>
 
 
+
   <div class="container" id="seccion-4">
     <div class="row">
-    <h2>El local</h2>
-    <p>Nuestro local es un refugio de verdad para los que somos fanáticos de la música en formato analógico. En el '80, un tipo re apasionado por los vinilos lo fundó con la idea de compartir su amor por la música con la gente del barrio, y mirá que seguimos a full con esa dedicación musical a través de los años.</p>
-    <p>Las imágenes te van a llevar a recorrer las estanterías llenas de discos de vinilo, los rincones re copados donde nos juntamos los fanáticos de la música, y las sesiones de escucha que armamos y que han hecho que se forjen lazos musicales y amistades que duran toda la vida. Vas a ver cómo fuimos cuidando esa tradición musical durante décadas y cómo nos hicimos parte de la movida musical del barrio.</p>
-<?php
-        foreach ($fotosLocal as $k => $imagen) {
-      ?>
+      <h2>El local</h2>
+      <p>Nuestro local es un refugio de verdad para los que somos fanáticos de la música en formato analógico. En el
+        '80, un tipo re apasionado por los vinilos lo fundó con la idea de compartir su amor por la música con la gente
+        del barrio, y mirá que seguimos a full con esa dedicación musical a través de los años.</p>
+      <p>Las imágenes te van a llevar a recorrer las estanterías llenas de discos de vinilo, los rincones re copados
+        donde nos juntamos los fanáticos de la música, y las sesiones de escucha que armamos y que han hecho que se
+        forjen lazos musicales y amistades que duran toda la vida. Vas a ver cómo fuimos cuidando esa tradición musical
+        durante décadas y cómo nos hicimos parte de la movida musical del barrio.</p>
+      <?php
+      foreach ($fotosLocal as $k => $imagen) {
+        ?>
         <div class="col-12 col-md-3 col-lg-2">
           <div class="card">
             <img src="images/<?php echo $imagen['imagen'] ?>" class="card-img-top" alt="<?php echo $imagen['alt'] ?>"
               class="galeria-local">
           </div>
         </div>
-      <?php
-        }
+        <?php
+      }
       ?>
     </div>
   </div>
